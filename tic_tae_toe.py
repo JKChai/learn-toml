e46ca2ff56bfeecf2d119b5eebcfdf2692037174
Jin Kun Chai <chaijinkun@gmail.com>
2023-10-10 11:54:14 -0500

    Updated 2 Scripts

--- tic_tae_toe.py
+++ tic_tae_toe.py
@@ -1,3 +1,9 @@
+
+from pathlib import Path
 import tomli
 
+with open("tic_tac_toe.toml", mode="rb") as fp:
+    config = tomli.load(fp)
+
+tomli.loads(Path("tic_tac_toe.toml").read_text(encoding="utf-8"))
 
