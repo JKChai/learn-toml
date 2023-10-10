1a512d5d8ca0c8afb605cad5150666ea761fe276
Jin Kun Chai <chaijinkun@gmail.com>
2023-10-10 14:40:29 -0500

    Update tic_tac_toe.py

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,9 +1,11 @@
 from pathlib import Path
 import tomllib ## Part of Python 3.11 stdlib
 
-with open("tic_tac_toe.toml", mode="rb") as fp:
-    config = tomllib.load(fp)
-print(config)
+print(Path(__file__).parent)
 
-print(tomllib.loads(Path("tic_tac_toe.toml").read_text(encoding="utf-8")))
+# with open("tic_tac_toe.toml", mode="rb") as fp:
+#     config = tomllib.load(fp)
+# print(config)
+
+# print(tomllib.loads(Path("tic_tac_toe.toml").read_text(encoding="utf-8")))
 
