b760a781cc60623ccfdbebd10fe9778c870bc76f
Jin Kun Chai <chaijinkun@gmail.com>
2023-10-10 12:18:34 -0500

    Update tic_tae_toe.py

--- tic_tae_toe.py
+++ tic_tae_toe.py
@@ -4,5 +4,6 @@ import tomllib ## Part of Python 3.11 stdlib
 with open("tic_tac_toe.toml", mode="rb") as fp:
     config = tomllib.load(fp)
 
-tomllib.loads(Path("tic_tac_toe.toml").read_text(encoding="utf-8"))
+print(Path("."))
+# tomllib.loads(Path("tic_tac_toe.toml").read_text(encoding="utf-8"))
 
